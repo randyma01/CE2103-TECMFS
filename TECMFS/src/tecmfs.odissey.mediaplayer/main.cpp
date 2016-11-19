6eb8764f774a016ddc59d53bda7a93da04e2a683
tavoGFC <gusfcarrera@hotmail.es>
2016-11-19 15:22:30 -0600

    Paquete de  Odissey MediaPlayer
    Agregar folder para el paquete del reproductor

---
+++ TECMFS/src/tecmfs.odissey.mediaplayer/main.cpp
@@ -0,0 +1,115 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 7 nov. 2016
+ *      Author: gustavo
+ */
+
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <strings.h>
+#include <fstream>
+#include <cstring>
+#include <cstddef>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+
+
+#include <cv.h>
+#include <highgui.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/video/video.hpp>
+#include <opencv2/opencv.hpp>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <net/if.h>
+#include <errno.h>
+#include <ifaddrs.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <sys/stat.h>
+
+
+
+#include <x86_64-linux-gnu/libavutil/imgutils.h>
+#include <x86_64-linux-gnu/libavcodec/avcodec.h>
+#include <x86_64-linux-gnu/libavformat/avformat.h>
+#include <x86_64-linux-gnu/libswscale/swscale.h>
+
+
+#include <iostream>
+#include <stdio.h>
+#include <inttypes.h>
+#include <bitset>
+
+
+#include <zlib.h>
+#include <zip.h>
+
+#include <vlc/libvlc.h>
+#include <vlc/vlc.h>
+
+#include <qmainwindow.h>
+#include <qaction.h>
+#include <qlayout.h>
+#include <qmenu.h>
+#include <qmenubar.h>
+#include <qstatusbar.h>
+#include <qtextedit.h>
+#include <qfile.h>
+#include <qdatastream.h>
+#include <qfiledialog.h>
+#include <qmessagebox.h>
+#include <qsignalmapper.h>
+#include <qapplication.h>
+#include <qpainter.h>
+#include <QMouseEvent>
+#include <qlineedit.h>
+#include <qcombobox.h>
+#include <qlabel.h>
+#include <qtoolbar.h>
+#include <qpushbutton.h>
+#include <qobjectdefs.h>
+#include <qapplication.h>
+#include <qmovie.h>
+
+
+#include "Converter.h"
+#include "mainWindow.h"
+
+
+
+
+typedef unsigned char BYTE;
+
+using namespace std;
+using namespace cv;
+
+
+int main (int argc, char *argv[]){
+//	QApplication app(argc, argv);
+//	mainWindow mainWindo;
+//	mainWindo.show();
+//	return app.exec();
+
+//	Converter conv;
+//	conv.loadVideo("videos/VID.avi");
+
+}
+
+
+
