b678e15f5dbb56a625105120a8c02764e62dd282
Randy <randyma01@gmail.com>
2016-11-21 15:01:52 -0600

    Upadte on code.

--- TECMFS/src/tecmfs.server/Parity.cpp
+++ TECMFS/src/tecmfs.server/Parity.cpp
@@ -7,14 +7,27 @@
 
 #include "Parity.h"
 
+/**
+ * Constructor.
+ */
 Parity::Parity() {
 }
 
+
+/**
+ * Destroyer.
+ */
 Parity::~Parity() {
 }
 
 
-
+/**
+ * receives two strings. Makes the XOR logical function,
+ * if A and B are different returns 1, else return 0.
+ *
+ * @param a char, b char
+ * @return binary string
+ */
 string Parity::XOR(char a, char b){
 	if(a==b){
 		return "0";
@@ -24,7 +37,12 @@ string Parity::XOR(char a, char b){
 	}
 }
 
-
+/**
+ * Receive a string and filled it with 1's.
+ *
+ * @param str string, cant int
+ * @return resp string
+ */
 string Parity::fillOnes(string str,int cant){
 	string resp=str;
 	for(int i=0; i<cant;i++){
@@ -33,6 +51,14 @@ string Parity::fillOnes(string str,int cant){
 	return resp;
 }
 
+/**
+ * Receives two string, and create the parity between them.
+ * In case that the string are not equal length, it automatically
+ * filles the shorter one with ones.
+ *
+ * @param a string, b string
+ * @return parity string
+ */
 string Parity::createParity(string a,string b){
 	if (a.length() > b.length()){
 		b= fillOnes(b,  (a.length()- b.length()) );
